def35c97a6a6419c241b9cf33e44a63ca3272684
Salma <salmanabih110@gmail.com>
2025-02-26 02:50:37 +0200

    reformat auth.service.ts

--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -7,29 +7,28 @@ import { jwtDecode } from 'jwt-decode';
   providedIn: 'root'
 })
 export class AuthService {
-userid:any;
+  userid: any;
 
-  constructor(private _HttpClient:HttpClient) {
+  constructor(private _HttpClient: HttpClient) {
 
   }
 
-setregister(userdata:object):Observable<any>
-{
-return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signup',userdata);
-}
-setlogin(userdata:{}):Observable<any>{
- return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signin',userdata);
-}
+  setregister(userdata: object): Observable<any> {
+    return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signup', userdata);
+  }
+  setlogin(userdata: {}): Observable<any> {
+    return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signin', userdata);
+  }
 
 
 
-decodeUser():void{
-  const encode=localStorage.getItem('usertoken')
-if(encode!=null){
-  const decode=jwtDecode(encode)
-  console.log(decode);
-  this.userid=decode
-  console.log(this.userid.id)
-}
-}
+  decodeUser(): void {
+    const encode = localStorage.getItem('usertoken')
+    if (encode != null) {
+      const decode = jwtDecode(encode)
+      console.log(decode);
+      this.userid = decode
+      console.log(this.userid.id)
+    }
+  }
 }
